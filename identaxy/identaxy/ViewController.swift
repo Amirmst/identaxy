79898d3284d6bdf0fb69cf8fff208283bbf1d668
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2019-10-01 19:04:30 -0500

    Added cocoapods for swiping cards and pop-bounce-button.

---
+++ identaxy/identaxy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  identaxy
+//
+//  Created by Amir Mostafavi on 10/1/19.
+//  Copyright © 2019 amir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
