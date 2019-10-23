9dbc21c70d993c1c74d065244ebff5fc968e3a40
Kevin Nguyen <ktn1234@yahoo.com>
2019-10-22 23:16:38 -0500

    Done with SwipeScreen functionality

--- identaxy/identaxy/Kolada/SwipingVC.swift
+++ identaxy/identaxy/Kolada/SwipingVC.swift
@@ -5,95 +5,75 @@
 //  Created by Kevin Nguyen on 10/20/19.
 //  Copyright © 2019 amir. All rights reserved.
 //
+//
+//  BackgroundAnimationViewController.swift
+//  Koloda
+//
+//  Created by Eugene Andreyev on 7/11/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
 
 import UIKit
-import pop
 import Koloda
+import pop
 
-private let numberOfCards: Int = 1
+private let numberOfCards: Int = 6
 private let frameAnimationSpringBounciness: CGFloat = 9
 private let frameAnimationSpringSpeed: CGFloat = 16
-private let kolodaCountOfVisibleCards = 1
-private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
+private let kolodaCountOfVisibleCards = 2
+private let kolodaAlphaValueSemiTransparent: CGFloat = 0
 
 class SwipingVC: UIViewController {
 
+        
+    @IBOutlet weak var kolodaView: CustomKolodaView!
     @IBOutlet weak var identaxyLabel: UILabel!
     
-    fileprivate var dataSource: [UIImage] = {
-        var array: [UIImage] = []
-        for index in 0..<numberOfCards { //change number of cards to cap it
-            array.append(UIImage(named: "Card_like_\(index + 1)")!) //Change to our file name
-        }
-        
-        return array
-    }()
-    
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
-    
-    @IBAction func informationButtonPressed(_ sender: Any) {
-        
-        
-    }
-    
-    @IBAction func settingsButtonPressed(_ sender: Any) {
-        
-    }
-    
-    @IBOutlet weak var kolodaView: CustomKolodaView!
-    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        overrideUserInterfaceStyle = .dark
-        identaxyLabel.textColor = UIColor.white
         kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
         kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
         kolodaView.delegate = self
         kolodaView.dataSource = self
         kolodaView.animator = BackgroundKolodaAnimator(koloda: kolodaView)
-        
+        identaxyLabel.textColor = UIColor.white
+        overrideUserInterfaceStyle = .dark
         self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
     }
     
-    @IBAction func leftButtonTapped(_ sender: Any) {
+    
+    //MARK: IBActions
+    @IBAction func leftButtonTapped() {
         kolodaView?.swipe(.left)
     }
     
-    @IBAction func rightButtonTapped(_ sender: Any) {
+    @IBAction func rightButtonTapped() {
         kolodaView?.swipe(.right)
     }
     
-    @IBAction func undoButtonTapped(_ sender: Any) {
+    @IBAction func undoButtonTapped() {
         kolodaView?.revertAction()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func infoButtonTapped(_ sender: Any) {
+        
     }
-    */
+    
+    @IBAction func settingsButtonTapped(_ sender: Any) {
+        
+    }
 }
-
-
 
 //MARK: KolodaViewDelegate
 extension SwipingVC: KolodaViewDelegate {
     
-    
-    
     func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        print("RELOAD")
         kolodaView.resetCurrentCardIndex()
     }
     
     func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
-//        UIApplication.shared.open(URL("https://www.google.com"), options: <#T##[UIApplication.OpenExternalURLOptionsKey : Any]#>, completionHandler: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+        UIApplication.shared.openURL(URL(string: "https://yalantis.com/")!)
     }
     
     func kolodaShouldApplyAppearAnimation(_ koloda: KolodaView) -> Bool {
@@ -120,7 +100,7 @@ extension SwipingVC: KolodaViewDelegate {
 extension SwipingVC: KolodaViewDataSource {
     
     func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
-        return .default
+        return .moderate
     }
     
     func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
@@ -128,8 +108,7 @@ extension SwipingVC: KolodaViewDataSource {
     }
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
-//        return UIImageView(image: UIImage(named: "cards_\(index + 1)"))
-        return UIImageView(image: UIImage(named: "User Pic"))
+        return UIImageView(image: UIImage(named: "User Pic\(index + 1)"))
     }
     
     func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
