c10c6408a195bf015c8e822eb73289199274eeb8
Ailyn Aguirre <ailyn7@wash.cs.utexas.edu>
2019-10-24 07:21:43 -0500

    Implemented Update Password

--- identaxy/identaxy/UpdatePasswordVC.swift
+++ identaxy/identaxy/UpdatePasswordVC.swift
@@ -23,16 +23,40 @@ class UpdatePasswordVC: IdentaxyHeader {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark
         self.setHeaderTitle(title: "Update Password")
+        forgotPassButton.setTitleColor(UIConstants.IDENTAXY_PINK, for: .normal)
+        currentPassTextField.setPlaceholder(placeholder: "Current password")
+        currentPassTextField.textColor = UIColor.gray
+        
+        newPassTextField.setPlaceholder(placeholder: "New password")
+        newPassTextField.textColor = UIColor.gray
+        
+        confirmPassTextField.setPlaceholder(placeholder: "Confirm password")
+        confirmPassTextField.textColor = UIColor.gray
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func forgotPassButtonPressed(_ sender: Any) {
-        
+        let alertVC = alertService.alert(title: "Please check your email!", message: "We sent you an email with password reset instruction", button: "OK")
+        present(alertVC, animated: true, completion: nil)
     }
     
     @IBAction func updateButtonPressed(_ sender: Any) {
-        
+        if(!currentPassTextField.text!.isEmpty && !newPassTextField.text!.isEmpty && !confirmPassTextField.text!.isEmpty) {
+            if(newPassTextField.text != confirmPassTextField.text) {
+                let alertVC = alertService.alert(title: "Error", message: "Passwords do not match", button: "Dismiss")
+                present(alertVC, animated: true, completion: nil)
+            } else {
+            let alertVC = alertService.alert(title: "Success!", message: "You have successfully changed your password", button: "OK")
+            present(alertVC, animated: true, completion: nil)
+            }
+        } else {
+            let alertVC = alertService.alert(title: "Error", message: "Please enter all fields", button: "Dismiss")
+            present(alertVC, animated: true, completion: nil)
+        }
     }
     
     // code to dismiss keyboard when user clicks on background
