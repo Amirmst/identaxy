e4a717d07cc2c3f54929168da6c18090aef190d3
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2019-10-24 10:53:18 -0500

    added all files. Next: storyboard

---
+++ identaxy/identaxy/Custom UI Elements/SettingAPI.swift
@@ -0,0 +1,28 @@
+//
+//  SettingAPI.swift
+//  identaxy
+//
+//  Created by Ailyn Aguirre on 10/20/19.
+//  Copyright © 2019 Ailyn Aguirre. All rights reserved.
+//
+
+import Foundation
+
+struct Setting {
+    let name:String?
+    let img:String?
+    let btnStr:String?
+}
+
+class SettingAPI {
+ static func getSettings() -> [Setting]{
+   let settings = [
+                    Setting(name: "Dark Mode", img: "night-mode", btnStr: " "),
+                    Setting(name: "Email address", img: "mail-icon", btnStr: ">"),
+                    Setting(name: "Password", img: "password", btnStr: ">"),
+                    Setting(name: "Help & Support", img: "help-support", btnStr: ">"),
+                    Setting(name: "About", img: "about", btnStr: ">")
+                  ]
+   return settings
+  }
+}
